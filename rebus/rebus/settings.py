bf1559af963a083b3b4d623abc5ed6c4a9bd49eb
Henoc Díaz <henocdz@gmail.com>
2015-04-12 00:28:36 -0500

    useless templates settings

--- rebus/rebus/settings.py
+++ rebus/rebus/settings.py
@@ -52,24 +52,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'rebus.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'rebus.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
